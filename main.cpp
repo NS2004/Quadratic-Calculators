f62ccc9f6e86ee0c1be82b192f38adf4c7147fae
NS2004 <48700224+NS2004@users.noreply.github.com>
2019-05-16 21:15:43 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,23 +8,30 @@ int main () {
   double a, b, c;
   char choice;
 
-  cout << "a";
-  cin >> a;
-  cout << "b";
-  cin >> b;
-  cout << "c";
-  cin >> c;
-
-//Discriminate (b^2 - 4ac)
-  double discriminate = pow(b, 2) - 4 * a * c;
-  cout << "Discriminate:\n" << discriminate;
-//Quadratic equation values of x 
-  double ans1 = ((-b) + (sqrt (discriminate))) / (2 * a);
-  double ans2 = ((-b) - (sqrt (discriminate))) / (2 * a);
-  if (ans1 != ans2) {
-    cout << "\nValues of x:\n" << ans1 << "\n" << ans2;
-  }
-  if (ans1 == ans2) {
-    cout << "\nValue of x:\n" << ans1;
-  }
+    while (choice !='n') {
+      cout << "a";
+      cin >> a;
+      cout << "b";
+      cin >> b;
+      cout << "c";
+      cin >> c;
+    
+    //Discriminate (b^2 - 4ac)
+      double discriminate = pow(b, 2) - 4 * a * c;
+      cout << "Discriminate:\n" << discriminate;
+    //Quadratic equation values of x 
+      double ans1 = ((-b) + (sqrt (discriminate))) / (2 * a);
+      double ans2 = ((-b) - (sqrt (discriminate))) / (2 * a);
+      if (ans1 != ans2) {
+        cout << "\nValues of x:\n" << ans1 << "\n" << ans2;
+      }
+      if (ans1 == ans2) {
+        cout << "\nValue of x:\n" << ans1;
+      }
+      cout << "\ny/n:";
+      cin >> choice;
+      if (choice == 'n') {
+          break;
+      }
+    }
 }
